9deccf7bc82fd09b7cae8be1f767aab3797f1844
Eunsoo.Oh <mochee1004@naver.com>
2024-04-18 09:48:29 +0900

    5,6주차 과제 실습 2 -2개의 박스가 같은 치수인지 확인하는 클래스입니다.

---
+++ src/Box.java
@@ -0,0 +1,16 @@
+
+public class Box {
+
+	public int width;
+	public int length;
+	public int height;
+	
+	public Box(int w, int l, int h) {
+		
+	}
+
+	public boolean isSameBox(Box obj) {
+		return true;
+	}
+	
+}
