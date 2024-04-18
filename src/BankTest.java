c8b90ef11bb996a4348c13391dd5b4a3d97eb123
Eunsoo.Oh <mochee1004@naver.com>
2024-04-18 09:45:09 +0900

    5,6주차 과제 실습 4 -은행 코드 테스트 클래스입니다.

---
+++ src/BankTest.java
@@ -0,0 +1,16 @@
+
+public class BankTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank[] bank = new Bank[3];
+		bank[0] = new BadBank();
+		bank[1] = new NormalBank();
+		bank[2] = new GoodBank();
+		
+		System.out.println("BadBank의 이자율 : " + bank[0].getInterestRate());
+		System.out.println("NormalBank의 이자율 : " + bank[1].getInterestRate());
+		System.out.println("GoodBank의 이자율 : " + bank[2].getInterestRate());
+	}
+
+}
