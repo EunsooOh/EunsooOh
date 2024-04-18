03cbeac341ae54ced9ebf6f4c43ea2cce93eef59
Eunsoo.Oh <mochee1004@naver.com>
2024-04-18 09:41:07 +0900

    5,6주차 과제 실습5 -고양이 자식 클래스입니다. -오버라이드 사용했습니다.

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -5,6 +5,8 @@ public class EvenOdd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          Scanner sc = new Scanner(System.in); 
+         
+         System.out.println("정수를 입력하시오.");
          int N = sc.nextInt();
          if( N % 2 ==0 )
          {
@@ -14,6 +16,7 @@ public class EvenOdd {
          {
         	 System.out.println("0이거나 홀수입니다.");
          }
+         System.out.println("시스템이 종료되었습니다.");
 	}
 
 }
